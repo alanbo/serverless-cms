038d3b36c51ad3b6cbaceb837a2e2fef060631ac
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-02-06 23:41:01 -0800

    adds function that stores image data in DynamoDb

---
+++ functions/resize-images/storeImageData.js
@@ -0,0 +1,28 @@
+import AWS from 'aws-sdk';
+const dynamodb = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
+var IMG_TABLE = process.env.IMG_TABLE;
+
+function storeImageData(metadata, filename, paths) {
+  const params = {
+    TableName: IMG_TABLE,
+    Item: {
+      filename: filename,
+      timestamp: (new Date().toJSON()).toString(),
+      gallery: metadata.gallery || 'general',
+      metadata,
+      paths
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    dynamodb.put(params, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve('success');
+      }
+    });
+  });
+}
+
+export default storeImageData;
