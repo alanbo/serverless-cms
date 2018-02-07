09c081fe30dd4a33945f530a67a4d588beb5444e
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-02-06 22:13:04 -0800

    completes module that loads the transformed images to s3

--- functions/resize-images.js
+++ functions/resize-images.js
@@ -1,34 +1,27 @@
 import AWS from 'aws-sdk';
 import getImage from './resize-images/get-image';
 import transformImages from './resize-images/transform-images';
+import uploadImages from './resize-images/upload-images';
 
 // eslint-disable-next-line import/prefer-default-export
 export const resizeImages = async function(event, context, callback) {
-  const src_bucket = event.Records[0].s3.bucket.name;
+  const bucket = event.Records[0].s3.bucket.name;
   const src_key = event.Records[0].s3.object.key;
   const filename = src_key.split('/').pop();
 
-
   try {
-    const image_buffer = (await getImage(src_bucket, src_key)).Body;
-    const transformed_images = await transformImages(image_buffer);
+    const s3_image = (await getImage(bucket, src_key));
+    const buffer = s3_image.Body;
+    const metadata = s3_image.Metadata;
+    const content_type = s3_image.ContentType;
+    const images = await transformImages(buffer);
+    const upload_data = { images, buffer, metadata, bucket, filename, content_type };
 
-    console.log('image data: ', transformed_images);
+    const stored_images = await uploadImages(upload_data);
+
   } catch(e) {
     console.log(e);
   }
 
   callback(null, 'Image successfully resized');
-
-
-
-
-
-
-  // p
-  //   .then(() => callback(null, {
-  //     message: 'Go Serverless Webpack (Ecma Script) v1.0! First module!',
-  //     event,
-  //   }))
-  //   .catch(e => callback(e));
 };
---
+++ functions/resize-images/upload-images.js
@@ -0,0 +1,34 @@
+/* global global */
+import AWS from 'aws-sdk';
+const s3 = new AWS.S3();
+
+function uploadOne(bucket, content_type, metadata, filename, { buffer, key }) {
+  const gallery = metadata.gallery || 'general';
+  const path = `images/${gallery}/${key}/${filename}`;
+
+  return new Promise((resolve, reject) => {
+    s3.putObject({
+        Bucket: bucket,
+        Key: path,
+        Body: buffer,
+        ContentType: content_type,
+        ACL: 'public-read',
+        Metadata: metadata
+    }, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ path, type: key });
+      }
+    });
+  });
+}
+
+const uploadImages = upload_data => {
+  const { images, buffer, metadata, bucket, filename, content_type } = upload_data;
+  const image_data = images.map( img => uploadOne(bucket, content_type, metadata, filename, img));
+
+  return Promise.all(image_data);
+};
+
+export default uploadImages;
