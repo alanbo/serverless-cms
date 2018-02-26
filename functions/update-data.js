acadbfcc9f82dfc01f3f263bf019329c781bc18c
alan.bogusiewicz <alan.bogusiewicz@camppacific.com>
2018-02-25 16:45:59 -0800

    adds remaining update rest verbs

--- functions/update-data.js
+++ functions/update-data.js
@@ -4,26 +4,28 @@ const dynamodb = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
 var FRAGMENTS_TABLE = process.env.FRAGMENTS_TABLE;
 
 module.exports.updateData = (event, context, cb) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(event)
-  };
+  const body = event.body ? JSON.parse(event.body) : {};
 
   switch (event.httpMethod) {
     case 'PUT':
+    case 'POST':
       const id = ObjectID().toString('utf8');
-
       const params = {
         TableName: FRAGMENTS_TABLE,
         Item: {
           id,
-          data: JSON.parse(event.body)
+          type: body.type || 'general',
+          data: body.data
         }
       };
 
       dynamodb.put(params, err => {
         if (err) {
-          cb(err);
+          console.log('err: ', err);
+          cb(null, {
+            statusCode: 400,
+            body: err.errorMessage
+          });
         } else {
           cb(null, {
             statusCode: 200,
@@ -32,9 +34,57 @@ module.exports.updateData = (event, context, cb) => {
         }
       });
       break;
+
     case 'PATCH':
+      const patchParams = {
+        TableName: FRAGMENTS_TABLE,
+        Key: event.pathParameters,
+        UpdateExpression: body.update,
+        ExpressionAttributeValues: body.values,
+        ReturnValues: 'ALL_NEW',
+        ConditionExpression: 'attribute_exists(id)'
+      };
+
+      if (body.names) {
+        patchParams.ExpressionAttributeNames = body.names;
+      }
+
+
+      dynamodb.update(patchParams, (err, data) => {
+        if (err) {
+          console.log('err: ', err);
+          cb(null, {
+            statusCode: 400,
+            body: err.message
+          });
+        } else {
+          cb(null, {
+            statusCode: 200,
+            body: JSON.stringify(data)
+          });
+        }
+      });
       break;
+
     case 'DELETE':
+      const delParams = {
+        TableName: FRAGMENTS_TABLE,
+        Key: event.pathParameters
+      };
+
+      dynamodb.delete(delParams, err => {
+        if (err) {
+          cb(null, {
+            statusCode: 400,
+            body: err.errorMessage
+          });
+        } else {
+          cb(null, {
+            statusCode: 200,
+            body: 'Item deleted'
+          });
+        }
+      });
       break;
   }
 }
