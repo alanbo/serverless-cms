5f9c48ac5af3f6e3679bd1f6a62ccefaac502567
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-03-03 00:26:56 -0800

    adds authomatic data for client cognito

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,19 @@
-function handler (data, serverless, options) {
-  console.log('Received Stack Output', data.CognitoARN)
+const path = require('path');
+const fs = require('fs');
+
+function handler ({ region, userPoolId, userPoolWebClientId }, serverless, options) {
+  const client_data = { region, userPoolId, userPoolWebClientId };
+  const file_content = `export default ${JSON.stringify(client_data, null, '  ')}`;
+  const file_path = path.join(__dirname, '../public/admin', 'cognito.js');
+
+  fs.writeFile(file_path, file_content, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    console.log('Sucessfully saved: ', client_data);
+  });
 }
 
 module.exports = { handler }
