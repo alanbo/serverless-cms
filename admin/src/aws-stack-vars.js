d3d0e68646ce7674e10dfb0323e691681b818af5
alan.bogusiewicz <alan.bogusiewicz@camppacific.com>
2018-06-15 00:02:51 -0700

    moves admin and updates cors for s3

---
+++ admin/src/aws-stack-vars.js
@@ -0,0 +1,4 @@
+export default {
+  "endpoint": "https://mk19gk45pk.execute-api.us-west-2.amazonaws.com/dev",
+  "graphqlApiId": "jlwwaaas5vcazlu5aummdaa64i"
+}
---
+++ admin/src/cognito.js
@@ -0,0 +1,5 @@
+export default {
+  "region": "us-west-2",
+  "userPoolId": "us-west-2_7mxUJNQrE",
+  "userPoolWebClientId": "4ihqu8fbu921s1n3tfabkto28b"
+}
