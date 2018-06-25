d117d4b3b40d414989e5ef98ab2675558f649960
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-06-24 22:20:29 -0700

    adds uploding/reading of images in admin dashboard

--- admin/src/components/main/Photos.js
+++ admin/src/components/main/Photos.js
@@ -1,33 +1,21 @@
 import React, { Component } from 'react';
 import { Storage } from 'aws-amplify';
-import { PhotoPicker, S3Image } from 'aws-amplify-react';
+import { PhotoPicker, S3Image, S3Album } from 'aws-amplify-react';
 
-class S3ImageUpload extends React.Component {
-  onChange(e) {
-      const file = e.target.files[0];
-      Storage.put('example.png', file, {
-          contentType: 'image/png'
-      })
-      .then (result => console.log(result))
+class Photos extends Component {
+
+  componentDidMount() {
+    Storage.list('temp/')
+      .then(result => console.log(result))
       .catch(err => console.log(err));
   }
 
   render() {
-      return (
-          <input
-              type="file" accept='image/png'
-              onChange={(e) => this.onChange(e)}
-          />
-      )
-  }
-}
-
-class Photos extends Component {
-  render() {
     return (
       <div>
         <h1>Photos</h1>
-        <S3ImageUpload />
+        {/* <S3Image path="temp/" picker /> */}
+        <S3Album path="temp/" picker />
       </div>
     );
   }
