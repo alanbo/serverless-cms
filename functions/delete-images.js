1c6e26c4ee3451f34faa1858a541fcbfa2c0c0ac
alan.bogusiewicz <alan.bogusiewicz@camppacific.com>
2018-07-03 09:29:36 -0700

    adds partial deleting functionality

--- functions/delete-images.js
+++ functions/delete-images.js
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk';
+import removeEach from './delete-images/remove-each';
 
 // eslint-disable-next-line import/prefer-default-export
 export const deleteImages = async function(event, context, callback) {
@@ -6,11 +7,15 @@ export const deleteImages = async function(event, context, callback) {
   const src_key = event.Records[0].s3.object.key;
   const filename = src_key.split('/').pop();
 
+
+
   try {
-    console.log(filename);
+    console.log(src_key);
+    await removeEach(bucket, src_key);
 
   } catch(e) {
     console.log(e);
+    callback(e);
   }
 
   callback(null, 'Image successfully resized');
