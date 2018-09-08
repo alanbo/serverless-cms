892c7a884aeb78feed955ba9816c5ef8d1a71c24
alan.bogusiewicz <alan.bogusiewicz@camppacific.com>
2018-09-08 15:49:54 -0700

    updates lambda to store images in one folder

--- functions/resize-images.js
+++ functions/resize-images.js
@@ -2,6 +2,7 @@ import AWS from 'aws-sdk';
 import getImage from './resize-images/get-image';
 import transformImages from './resize-images/transform-images';
 import uploadImages from './resize-images/upload-images';
+import settings from './resize-images/settings';
 import storeImageDataGraphQL from './resize-images/storeImageDataGraphQL';
 
 // eslint-disable-next-line import/prefer-default-export
@@ -10,15 +11,27 @@ export const resizeImages = async function(event, context, callback) {
   const src_key = event.Records[0].s3.object.key;
   const filename = src_key.split('/').pop();
 
+  const random_str = Math.round(Math.random() * 1000000000).toString(16);
+  const resized_filename_arr = filename.split('.');
+  const ext = resized_filename_arr.pop();
+  const resized_filename = resized_filename_arr.join('.');
+  const create_path = key => {
+    return {
+      type: key,
+      path: `public/images/${resized_filename}-${key}-${random_str}.${ext}`
+    };
+  };
+
+  const paths = Object.keys(settings).map(create_path);
+
   try {
     const s3_image = (await getImage(bucket, src_key));
     const buffer = s3_image.Body;
     const metadata = s3_image.Metadata;
     const content_type = s3_image.ContentType;
     const images = await transformImages(buffer);
-    const upload_data = { images, buffer, metadata, bucket, filename, content_type };
-    const stored_images = await uploadImages(upload_data);
-    await storeImageDataGraphQL(metadata, filename, stored_images);
+    await uploadImages({ images, paths, metadata, bucket, filename, content_type });
+    await storeImageDataGraphQL(metadata, filename, paths);
   } catch(e) {
     console.log(e);
   }
