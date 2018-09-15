4ec35026d839c1dc52eeaaf9f9c3f202bf016f35
alan.bogusiewicz <alan.bogusiewicz@camppacific.com>
2018-09-15 13:48:58 -0700

    sets basic redux structure

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+
+import root_reducer from './reducers/index';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(root_reducer);
+
 ReactDOM.render((
   <BrowserRouter>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </BrowserRouter>
 ), document.getElementById('root'))
 
---
+++ admin/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import imageList from './imageList';
+
+const serverless = combineReducers({
+  imageList
+});
+
+export default serverless;
