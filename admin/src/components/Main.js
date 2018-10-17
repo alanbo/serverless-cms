3c86754993d3e67e5c2673f5da88cf54193de9d4
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-10-16 17:34:06 -0700

    adds image gallery add dialog

--- admin/src/components/Main.js
+++ admin/src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
-import { Home, Pages, Blog, Photos, Fragments, Thrash, Settings } from './main/index'
+import { Home, Pages, Blog, Photos, Fragments, Thrash, Settings, GallerySection } from './main/index'
 import Gallery from './main/photos/Gallery';
 
 const Main = () => (
@@ -9,7 +9,7 @@ const Main = () => (
       <Route exact path='/' component={Home}/>
       <Route path='/pages' component={Pages}/>
       <Route path='/blog' component={Blog}/>
-      <Route path='/photos/:gallery_name' component={Gallery}/>
+      <Route path='/photos/:gallery_name' component={GallerySection}/>
       <Route path='/photos' component={Photos}/>
       <Route path='/fragments' component={Fragments}/>
       <Route path='/thrash' component={Thrash}/>
---
+++ admin/src/components/main/GallerySection.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import AddToGalleryDialog from './gallery-section/AddToGalleryDialog';
+import Gallery from './photos/Gallery';
+import { styles } from './Photos';
+
+
+class GallerySectionClass extends Component {
+    constructor() {
+        super();
+        this.state = {
+            add_dialog_open: false
+        }
+    }
+
+    addDialog() {
+        console.log('adding');
+        this.setState({
+            add_dialog_open: true
+        });
+    }
+
+    closeDialog = () => {
+        this.setState({
+            add_dialog_open: false
+        });
+    }
+
+    render() {
+        const { gallery_name } = this.props.match.params;
+        const { classes } = this.props;
+        console.log(gallery_name);
+
+        return (
+            <div>
+                <Gallery gallery_name={ gallery_name } />
+                <Button variant="fab" color="primary" aria-label="add" className= { classes.button } onClick={ this.addDialog.bind(this) }>
+                    <AddIcon />
+                </Button>
+
+                {
+                    this.state.add_dialog_open
+                        ? <AddToGalleryDialog onClose={ this.closeDialog } onCancel={ this.closeDialog } />
+                        : null
+                }
+            </div>
+        );
+    };
+}
+
+const GallerySection = withStyles(styles)(GallerySectionClass);
+export { GallerySection };
