66b81160cf880c7b4278960142c2115a4647901c
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-10-26 21:48:33 -0700

    adds ability to add texts

---
+++ admin/src/components/main/fragments/common/TextList.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 400,
+  },
+});
+
+
+function TextList(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Snippet</TableCell>
+            <TableCell>Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.data.map(n => {
+            return (
+              <TableRow key={n.id}>
+                <TableCell component="th" scope="row">
+                  {n.snippet}...
+                </TableCell>
+                <TableCell>
+                  <IconButton aria-label="Delete" onClick={() => props.onDelete(n.id)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+export default withStyles(styles)(TextList);
