775236c76878662509442aa22753c44c36fc18bb
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-10-28 18:08:44 -0700

    adds front-end for text editing

--- admin/src/components/main/fragments/common/TextList.js
+++ admin/src/components/main/fragments/common/TextList.js
@@ -8,6 +8,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 import { connect } from 'react-redux';
 
 import * as actionCreators from '../../../../actions/index';
@@ -33,6 +34,7 @@ function TextList(props) {
         <TableHead>
           <TableRow>
             <TableCell>Snippet</TableCell>
+            <TableCell>Edit</TableCell>
             <TableCell>Delete</TableCell>
           </TableRow>
         </TableHead>
@@ -42,6 +44,11 @@ function TextList(props) {
               <TableRow key={n.id}>
                 <TableCell component="th" scope="row">
                   {n.snippet}...
+                </TableCell>
+                <TableCell>
+                  <IconButton aria-label="Delete" onClick={() => props.onEdit(n)}>
+                    <EditIcon />
+                  </IconButton>
                 </TableCell>
                 <TableCell>
                   <IconButton aria-label="Delete" onClick={() => props.removeText(n.id)}>
