fc19e9faa31f866c5c96cb0822a8d7d4489ce958
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-11-09 15:59:49 -0800

    plugs in config file s3 to graphql query

---
+++ functions/resolvePageType.js
@@ -0,0 +1,18 @@
+var AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+export const resolvePageType = async (event, context, callback) => {
+  const params = {
+    Bucket: process.env.BUCKET,
+    Key: 'templates/pages.json',
+  };
+
+  const data = await s3.getObject(params).promise();
+  const pages_data = JSON.parse(data.Body.toString('utf8'));
+  const page_types = Object.keys(pages_data);
+
+  return page_types.map(name => ({
+    name,
+    inputs: pages_data[name].inputs
+  }))
+}
