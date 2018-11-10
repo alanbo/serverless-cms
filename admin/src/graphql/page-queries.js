5594e7f3d91ec9b69b4c4908df62c419e494e323
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-11-10 11:14:31 -0800

    adds page related queries

--- admin/src/graphql/page-queries.js
+++ admin/src/graphql/page-queries.js
@@ -10,3 +10,33 @@ export const getPageTypeList = `
     }
   } 
 `;
+
+export const putPage = `
+  mutation putPage($name: String!, $type: String!, $fragments: [ID]) {
+    putPage(name: $name, type: $type, fragments: $fragments) {
+      id
+      name
+      fragments
+    }
+  }
+`;
+
+export const getPageList = `
+  {
+    getPageList {
+      id
+      name
+      fragments
+    }
+  }
+`;
+
+export const removePage = `
+  mutation removePage($id: ID!) {
+    removePage(id: $id) {
+      id
+      name
+      fragments
+    }
+  }
+`;
