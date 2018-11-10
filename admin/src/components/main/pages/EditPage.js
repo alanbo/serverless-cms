48b98224c8b01428f58289292e71ab8040c96f4a
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-11-10 14:17:22 -0800

    adds button that redirect to add new page

--- admin/src/components/main/pages/EditPage.js
+++ admin/src/components/main/pages/EditPage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class EditPage extends Component {
   render() {
     const name = this.props.match.params.page_name;
-    const id = this.props.location.state.id;
+    const id = this.props.location.state ? this.props.location.state.id : null;
     console.log(id);
     console.log(name);
 
