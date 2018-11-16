1bd5f5e87903320d66d6d05392d9473213266479
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-11-15 22:33:12 -0800

    updates webpack and plugs in pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require('path');
 // eslint-disable-next-line import/no-unresolved
 const slsw = require('serverless-webpack');
+// const nodeExternals = require('webpack-node-externals');
+
+// console.log(node)
+// externals: [nodeExternals({
+//   whitelist: ['aws-appsync', 'ramda', 'graphql-tag']
+// })],
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+  mode: 'development',
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
       loader: 'babel-loader',
       include: __dirname,
       exclude: /node_modules/,
-      query: {
-        plugins: ['transform-runtime'],
-        presets: ['babel-preset-env'],
+      options: {
+        presets: [
+          // 'react',
+          // 'stage-0',
+          ['env', { targets: { node: '8.10' } }]
+        ]
       }
     }],
   },
@@ -21,5 +31,8 @@ module.exports = {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
+  },
+  externals: {
+    pug: 'pug'
   },
 };
