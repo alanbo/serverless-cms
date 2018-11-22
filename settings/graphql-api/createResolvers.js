34ee220c03ead324f742704533c845d439bcc865
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-11-21 17:33:22 -0800

    completes resolver cleanup

--- settings/graphql-api/createResolvers.js
+++ settings/graphql-api/createResolvers.js
@@ -1,16 +1,41 @@
 const getTemplate = require('./create-resolvers/getTemplate');
 const getListTemplate = require('./create-resolvers/getListTemplate');
 const putTemplate = require('./create-resolvers/putTemplate');
+const nestedListTemplate = require('./create-resolvers/nestedListTemplate');
 
-const TYPES = ['Text', 'Gallery', 'Image', 'Menu', 'Page'];
+const TYPES = [
+  {
+    type: 'Text'
+  },
+  {
+    type: 'Gallery',
+    nested_list_fields: ['images']
+  },
+  {
+    type: 'Image'
+  },
+  {
+    type: 'Menu'
+  },
+  {
+    type: 'Page'
+  }
+];
 
-module.exports = () => {
+module.exports = serverless => {
+  console.log(serverless.service.service);
   const Resources = {};
 
-  TYPES.forEach(type => {
+  TYPES.forEach(({ type, nested_list_fields }) => {
     Resources[`get${type}`] = getTemplate(type);
-    Resources[`get${type}List`] = getListTemplate(type);
+    Resources[`get${type}List`] = getListTemplate(type, serverless.service.service);
     Resources[`put${type}`] = putTemplate(type);
+
+    if (Array.isArray(nested_list_fields)) {
+      nested_list_fields.forEach(field => {
+        Resources[`resolver${type}${field}`] = nestedListTemplate(type, field);
+      });
+    }
   });
 
   return {
