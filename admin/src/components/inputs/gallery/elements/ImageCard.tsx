ede147592ddcbddb27a73226f8cbbc149e732065
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-12-06 09:44:39 -0800

    converts ImageCard and S3ImageUpload to typescript

--- admin/src/components/inputs/gallery/elements/ImageCard.tsx
+++ admin/src/components/inputs/gallery/elements/ImageCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, createStyles, WithStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,7 +7,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const styles = {
+const styles = createStyles({
   card: {
     maxWidth: 345,
   },
@@ -16,22 +15,26 @@ const styles = {
     // ⚠️ object-fit is not supported by IE11.
     objectFit: 'cover',
   },
-};
+});
 
-function ImgMediaCard(props) {
+interface Props extends WithStyles<typeof styles> {
+  path: string,
+  filename: string
+}
+
+function ImgMediaCard(props: Props) {
   const { classes } = props;
   return (
     <Card className={classes.card}>
       <CardMedia
         component="img"
         className={classes.media}
-        height="auto"
-        image={ this.props.path }
-        title={ this.props.filename }
+        image={props.path}
+        title={props.filename}
       />
       <CardContent>
         <Typography gutterBottom variant="headline" component="h2">
-          { this.props.filename }
+          {props.filename}
         </Typography>
         {/* <Typography component="p">
           Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
@@ -49,9 +52,5 @@ function ImgMediaCard(props) {
     </Card>
   );
 }
-
-ImgMediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(ImgMediaCard);
