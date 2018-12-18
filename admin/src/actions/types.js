e8df2f9bf52cfbc9aa6581d4283f81510c621693
Alan Bogusiewicz <abogusiewicz@gmail.com>
2018-12-17 20:55:43 -0800

    adds option that prevents deletion of elements that are related to by other fragments

--- admin/src/actions/types.js
+++ admin/src/actions/types.js
@@ -4,4 +4,6 @@ export const put_fragment = 'PUT_FRAGMENT';
 export const remove_fragment = 'REMOVE_FRAGMENT';
 export const resize_images = 'RESIZE_IMAGES';
 export const restore_fragment = 'RESTORE_FRAGMENT';
+export const delete_occurs_in_error = 'DELETE_OCCURS_IN_ERROR';
+export const clear_notification = 'CLEAR_NOTIFICATION';
 
--- admin/src/reducers/index.js
+++ admin/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import pageTypeList from './pageTypeList';
 import fragments from './fragments';
+import notification from './notification';
 
 const serverless = combineReducers({
   page_type_config: pageTypeList,
-  fragments
+  fragments,
+  notification
 });
 
 export default serverless;
