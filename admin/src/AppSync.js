f8b180fed402167e0f15300043ada3577fc1f2c2
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-01-02 10:58:58 -0800

    removes dynamic stack vars from the repo

--- admin/src/AppSync.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "graphqlEndpoint": "https://qic2egmwl5bvzixhgkr4sm3g3y.appsync-api.us-west-2.amazonaws.com/graphql",
-  "region": "us-west-2",
-  "authenticationType": "AMAZON_COGNITO_USER_POOLS",
-  "apiKey": "da2-26d7xypraveapfeje32zqip5pm"
-}
--- admin/src/cognito.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "region": "us-west-2",
-  "userPoolId": "us-west-2_t2Uvfl48i",
-  "userPoolWebClientId": "347p1eirq6qsodi6e9gmd0i32r",
-  "identityPoolId": "us-west-2:9dc251b0-3c3f-44cf-863d-705af47ceadc"
-}
