eaf54bf6b319ad16e5617d10905a86db8c2c1f4f
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-05-27 18:17:42 -0700

    adds basic test for Backups component

--- admin/src/components/Backups/Backups.tsx
+++ admin/src/components/Backups/Backups.tsx
@@ -3,15 +3,19 @@ import React, { Component } from 'react';
 interface Props {
   restoreFromBackup: (iso_string: string) => void,
   deleteBackups: (iso_string: string[]) => void,
-  backups: string[],
+  backups?: string[],
   backupData: () => void
 }
 
 class Backups extends Component<Props> {
   _backupList = () => {
-    const { restoreFromBackup, deleteBackups } = this.props;
+    const { restoreFromBackup, deleteBackups, backups } = this.props;
 
-    return this.props.backups.map(backup => (
+    if (!backups) {
+      return null;
+    }
+
+    return backups.map(backup => (
       <div key={backup}>
         <p >{backup}</p>
         <button onClick={restoreFromBackup.bind(null, backup)}>restore</button>
