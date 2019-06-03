5e01abf6bf8711bfb6dbb6130599e564a0f2922b
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-06-03 16:04:49 -0700

    fixes env variables issue

--- back_end/scripts/graphql/createFunctionResolvers.js
+++ back_end/scripts/graphql/createFunctionResolvers.js
@@ -9,41 +9,11 @@ module.exports = serverless => {
     // only ts files
     .filter(filename => filename.endsWith('.ts'))
     // remove ts extension
-    .map(filename => filename.slice(0, filename.length - 3))
-
-  // adds resolver and datasource for each filename
-  queries.forEach(name => {
-    functions[name] = {
-      handler: `functions/queries/${name}.handler`,
-      role: 'genericFunctionRole',
-      // TO DO: those values shouldn't be hardcoded
-      // create an option to configure them
-      timeout: 10,
-      memorySize: 2048,
-      environment: {
-        BUCKET: "${file(../slscms-config.yml):bucket_name}",
-        FRAGMENTS_TABLE: "${self:service}-fragments",
-        REGION: "${self:provider.region}",
-        GRAPHQL: {
-          "Fn::GetAtt": [
-            "graphQLApi",
-            "GraphQLUrl"
-          ]
-        }
-      }
-    };
-  });
-
-  const mutations = (fs.readdirSync(path.resolve(__dirname, '../../functions/mutations')) || [])
-    // only ts files
-    .filter(filename => filename.endsWith('.ts'))
-    // remove ts extension
     .map(filename => filename.slice(0, filename.length - 3));
 
-  // adds resolver and datasource for each filename
-  mutations.forEach(name => {
-    functions[name] = {
-      handler: `functions/mutations/${name}.handler`,
+  function createLambdaConfig(name, is_mutation = false) {
+    return {
+      handler: `functions/${is_mutation ? 'mutations' : 'queries'}/${name}.handler`,
       role: 'genericFunctionRole',
       // TO DO: those values shouldn't be hardcoded
       // create an option to configure them
@@ -61,7 +31,23 @@ module.exports = serverless => {
           ]
         }
       }
-    };
+    }
+  }
+
+  // adds resolver and datasource for each filename
+  queries.forEach(name => {
+    functions[name] = createLambdaConfig(name);
+  });
+
+  const mutations = (fs.readdirSync(path.resolve(__dirname, '../../functions/mutations')) || [])
+    // only ts files
+    .filter(filename => filename.endsWith('.ts'))
+    // remove ts extension
+    .map(filename => filename.slice(0, filename.length - 3));
+
+  // adds resolver and datasource for each filename
+  mutations.forEach(name => {
+    functions[name] = createLambdaConfig(name, true);
   });
 
   return functions;
